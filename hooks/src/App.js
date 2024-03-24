4a7631cc567397248a551d5f3158a252a2b1d26b
yash singla <yash.singla@logicladder.com>
2024-03-24 09:32:38 +0530

    FirstCommit

---
+++ hooks/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import HookUseState from './HookUseState'
+const App = () => {
+  return (
+    <>
+      <HookUseState />
+    </>
+  )
+}
+
+export default App
---
+++ hooks/src/HookUseState.jsx
@@ -0,0 +1,14 @@
+import React,{useState} from 'react'
+
+const HookUseState = () => {
+  return (
+    
+    <>
+     <button>-</button>
+     <span>0</span>
+     <button>+</button> 
+    </>
+  )
+}
+
+export default HookUseState
